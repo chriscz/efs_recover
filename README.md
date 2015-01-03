fef780705ed61a81a46fef6bfa245fe5a08b7c7e
Chris Coetzee <chriscz93@gmail.com>
2015-01-03 19:08:33 +0200

    Add automation of efs image creation

--- README.md
+++ README.md
@@ -1,11 +1,64 @@
-INSTRUCTIONS
+What is this tool?
+==================
+A script that attempts to extract nv_data from a 
+partition image. 
+The discussion below is applicable to the i9300 intl. GSM. 
+However, it may be relevant to other devices as well.
+
+When will it work?
+==================
+The script should work in the following cases
+- You have accidentally formatted your EFS partition incorrectly, but haven't written
+anything to it (for example, when it's been formatted through 
+[https://play.google.com/store/apps/details?id=org.azasoft.free.swapper&hl=en](Swapper for Root)) and thus `corrupted`.
+- You have a corrupted the EFS partition through some other means
+- Possibly when the phone gets stuck during boot 
+Corrupted EFS partition
+--------------------------------
+You know that the EFS partition has been corrupted when,
+    - You are stuck and cannot boot into your ROM  (mine was stuck at the bootloader `Samsung i9300` when I had CyanogenMod installed)
+    - If you're on stock, you are stuck at the Samsung ROM logo with a flashing blue indicater light and you should see the following 
+      error when you boot into the stock Samsung recovery:
+    >
+    > E:failed to mount /efs (Invalid argument)
+    >
+
+What about 0049 or 0000 IMEI?
+=============================
+The script will not be of any help, since the device has already restored
+its default EFS files. 
+
+On what has it been tested?
+===========================
+I have only tested the tool on i9300 intl. (Samsung S3), GSM
+with EFSv2. It may or may not work with EFSv1.
+
+Script Reqirements
+==================
+- Ubuntu / Debian / other Linux operating system
+- Python 2.7
+
+Instructions
 =============
-1. Make a full efs backup, even though you may have formatted
-the partition
-    
+0. Install a custom recovery, I used CWM (You can skip this step if you have already done so)
+1. Make a full efs backup from a terminal emulator (I used [http://forum.xda-developers.com/showthread.php?t=1646108](AROMA)), 
     >
     > dd if=/dev/block/mmcblk0p3 of=/extSdcard/efs_broken.img
     >
+2. Place partition backup on your computer
+3. Format your EFS partition using a terminal emulator from recovery (Again, I used AROMA)
+    >
+    > mke2fs /dev/block/mmcblk0p3
+    >
+4. Restart the phone, you should now be able to boot into your phone.
+   Wait till you reach the homescreen then continue with the instructions
 
-2. Place partition backup on the computer
-3.  
+5. Boot back into recovery, then backup your EFS partion again
+    >
+    > dd if=/dev/block/mmcblk0p3 of=/extSdcard/efs_default.img
+    >
+   This backup provides us with a working default which we shall use later
+6. Execute the following on your computer
+    >
+    > python efs_recover.py efs_broken.img
+    >
--- efs_recover.py
+++ efs_recover.py
@@ -3,6 +3,10 @@ import os
 import mmap
 import re
 import md5
+import subprocess
+import shutil
+import errno
+import time
 
 NV_DATA_BYTES = 2048*1024
 START_BYTES = '32'
@@ -10,11 +14,51 @@ GAP_MODEM = str(int('5c', 16))
 NV_END       = '\xff'
 NAME_NV_DATA = 'nv_data.bin'
 NAME_CHECKSUM= 'nv_data.bin.md5'
+P_NV_HEADER = re.compile(r'\xcc{' + START_BYTES +r'}.{' + GAP_MODEM + r'}\x20{4}(?P<modem>.{53})', re.DOTALL)
 
 DEBUG = False
+
 def debug(*args):
     if DEBUG:
         print ' '.join([repr(i) for i in args])
+
+def generate_md5sum(data, salt="Samsung_Android_RIL"):
+    checksum = md5.new(data)
+    checksum.update(salt)
+    return checksum.hexdigest()    
+
+def is_valid_nv_data(data, modem_name):
+    if not modem_name:
+        debug('Modem name does not exist')
+        return False
+    if 'undefined' in modem_name.lower():
+        debug('`undefined` in modem name')
+        return False
+    if not data[-1] == NV_END:
+        debug("nv_data has bad ending:", repr(data[-1]))
+        return False
+    return True
+
+class NvData(object):
+    def __init__(self, data):
+        self.modem = P_NV_HEADER.search(data)
+        if self.modem:
+            self.modem = self.modem.groupdict()['modem'].strip()
+        self.checksum = generate_md5sum(data)
+        self.data = data
+        self.valid = is_valid_nv_data(data, self.modem)
+
+    def save_data(self, path, name='nv_data.bin'):
+        filename = os.path.join(path, name)
+        with open(filename, 'w+b') as f:
+            f.write(self.data)
+            f.flush()
+        
+    def save_checksum(self, path, name='nv_data.bin.md5'):
+        filename = os.path.join(path, name)
+        with open(filename, 'w') as f:
+            f.write(self.checksum)
+            f.flush()
 
 def recursive_remove(folder):
     for the_file in os.listdir(folder):
@@ -34,20 +78,6 @@ def check_and_prep_recover_dir(path, dirname='recovered_efs'):
         recursive_remove(dirpath)
     return dirpath
 
-def generate_md5sum(data, salt="Samsung_Android_RIL"):
-    checksum = md5.new(data)
-    checksum.update(salt)
-    return checksum.hexdigest()    
-
-def is_valid_nv_data(data, modem_name):
-    if 'undefined' in modem_name.lower():
-        debug('`undefined` in modem name')
-        return False
-    if not data[-1] == NV_END:
-        debug("nv_data has bad ending:", repr(data[-1]))
-        return False
-    return True
-
 def read_stream(stream, start, count):
     old = stream.tell()
     stream.seek(start)
@@ -55,40 +85,86 @@ def read_stream(stream, start, count):
     stream.seek(old)
     return data
   
-def save_nv_data(data, path, suffix='', md5=True):
-    nv_data = "{}.{}".format(NAME_NV_DATA, suffix)
-    nv_sum = "{}.{}".format(NAME_CHECKSUM, suffix)
-
-    nv_data = os.path.join(path, nv_data)
-    nv_sum = os.path.join(path, nv_sum)
-    
-    with open(nv_data, 'w+b') as f:
-        f.write(data)
-    if md5:
-        checksum = generate_md5sum(data)
-        with open(nv_sum, 'w') as f:
-            f.write(checksum)
-
-def main():
+def extract_nv_data(efs_dump):
     here = os.getcwd()
     recover_dir = check_and_prep_recover_dir(here)
-    p_starting = re.compile(r'\xcc{' + START_BYTES +r'}.{' + GAP_MODEM + r'}\x20{4}(?P<modem>.{53})', re.DOTALL)
-    filename = sys.argv[1]
+    filename = efs_dump
+    extracted = []
     with open(filename, 'r+b') as f:
         mm = mmap.mmap(f.fileno(), 0)
         count = 0
-        for match in p_starting.finditer(mm):
-            modem_name = match.groupdict()['modem'].strip()
+        for match in P_NV_HEADER.finditer(mm):
             start = match.start(0)
-            # Construct the save-to path
-            nv_data = read_stream(mm, start, NV_DATA_BYTES)
-            if not is_valid_nv_data(nv_data, modem_name):
+            data = read_stream(mm, start, NV_DATA_BYTES)
+            nv_data = NvData(data)
+            if not nv_data.valid:
                 continue
-            save_nv_data(nv_data, recover_dir, suffix=count)
-            print "MODEM: ", modem_name
-            # continue from where we stopped
+            nv_data.save_data(recover_dir, name='nv_data.bin.' + str(count))
+            nv_data.save_checksum(recover_dir, name='nv_data.bin.md5.' + str(count))
+            extracted.append(nv_data)
             count += 1
-        print "FOUND: ", count, "nv_data.bin"
+        print "FOUND: ", count, "possible nv_data.bin"
+    return extracted
 
+def mount_loop_device(image_filename, path):
+    loop = subprocess.check_output(['sudo', 'losetup', '-f']).strip()
+    if subprocess.call(['sudo', 'losetup', loop, image_filename]) != 0:
+        raise Exception("Failed to setup loop device")
+    
+    if subprocess.call(['sudo', 'mount', loop, path]) != 0:
+        raise Exception("Failed to mount loop device")
+    return loop
+    
+def unmount_loop_device(loop_path):
+    if subprocess.call(['sudo', 'umount', loop_path]) != 0:
+        raise Exception("Failed to unmount loop device")
+
+    if subprocess.call(['sudo', 'losetup', '--detach', loop_path]) != 0:
+        raise Exception("Failed to detach loop device")
+
+def remove_file(path):
+    try :
+        os.remove(path)
+    except OSError as e:
+        if e.errno != errno.ENOENT:
+            raise e
+        
+def update_default_image(nv_data, directory):
+    to_remove = ['.nv_core.bak', '.nv_data.bak', 'nv_data.bin', 'nv.log', '.nv_core']
+    bak_bin_path = '.nv_data.bak'
+
+    md5ext = '.md5'
+    for i in to_remove:
+        remove_file(i)
+        remove_file(i + md5ext)
+
+    nv_data.save_data(directory)    
+    nv_data.save_checksum(directory)
+    nv_data.save_data(directory, name='.nv_data.bak')
+    nv_data.save_checksum(directory, name='.nv_data.bak.md5')
+
+    with open(os.path.join(directory, 'FactoryApp/keystr'), 'w') as f:
+        f.write('ON')
+        f.flush()
+
+    with open(os.path.join(directory, 'FactoryApp/factorymode'), 'w') as f:
+        f.write('ON')
+        f.flush()
+    time.sleep(2)
+
+def main():
+    here = os.getcwd()
+    mount_path = os.path.join(here, 'loop_mount')
+    if not os.path.exists(mount_path):
+        os.mkdir(mount_path)
+    extracted = extract_nv_data(sys.argv[1])
+    default_img = os.path.abspath(sys.argv[2])
+
+    for i, nv_data in enumerate(extracted):
+        image = os.path.join(here, 'updated_image_' + str(i) + '.img')
+        shutil.copy(default_img, image)
+        loop_path = mount_loop_device(image, mount_path)
+        update_default_image(nv_data, mount_path)
+        unmount_loop_device(loop_path)
 if __name__ == "__main__":
     main()
